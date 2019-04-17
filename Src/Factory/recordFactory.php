6cc720dd31e67bee78a608bd4502d5e43ec94a09
Akshesh Patel <47195718+Akshes@users.noreply.github.com>
2019-04-17 16:21:26 -0400

    Created record factory

---
+++ Src/Factory/recordFactory.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sanja
+ * Date: 4/17/2019
+ * Time: 4:20 PM
+ */
+
+namespace Akshes\Factory;
+use Akshes\models\record;
+
+
+
+class recordFactory
+{
+
+}
