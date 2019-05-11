a7dc0ab001b2178cc02eda9f5487890a9ab0b5c4
soham <sy357@njit.edu>
2019-05-11 17:06:11 -0400

    Created a php file to upload the table into onto html

---
+++ Src/html/ImportTable.php
@@ -0,0 +1,9 @@
+<?php
+function Import($class) {
+    include $class . '.php';
+}
+spl_autoload_register('Import');
+$file = new File();
+$array = $file::readCSVtoArray("../data/shoe.csv");
+$obj = new CreateSQLTable();
+$obj->createTaskTable($array[1],$array[0]);
